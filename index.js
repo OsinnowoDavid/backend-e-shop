55513caa4d7276696baae504dd9543494159cffa
OsinnowoDavid <davidadewale151@gmail.com>
2025-01-24 04:58:02 -0800

    push

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import connectdb from '../config/mongodb.js'; // Ensure this path is correct
-import connectcloudinary from '../config/cloudinary.js';
-import userrouter from '../routes/userroute.js';
-import productrouter from '../routes/productroute.js';
-import cartroute from '../routes/cartroute.js';
-import categoryrouter from '../routes/categoryroutes.js';
-import flashproductroute from '../routes/flashproductroutes.js';
-import hotproductroutes from '../routes/hotproductroutes.js';
+import connectdb from './config/mongodb.js'; // Ensure this path is correct
+import connectcloudinary from './config/cloudinary.js';
+import userrouter from './routes/userroute.js';
+import productrouter from './routes/productroute.js';
+import cartroute from './routes/cartroute.js';
+import categoryrouter from './routes/categoryroutes.js';
+import flashproductroute from './routes/flashproductroutes.js';
+import hotproductroutes from './routes/hotproductroutes.js';
 
 // Load environment variables
 dotenv.config();
